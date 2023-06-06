76448aacdd701019c07675c0f709d5550c9b9e56
Nemanja <nemanja.stosicc@gmail.com>
2023-06-06 12:59:10 +0200

    adding

---
+++ main.js
@@ -0,0 +1,36 @@
+
+let peopleCounter = document.getElementById('people-counter');
+let previousEntries = document.getElementById('save-entries');
+
+let count = 0;
+
+//textContent is aware of spaces
+function increment() {
+    count++;
+    peopleCounter.textContent = count;
+}
+
+function save() {
+    let temp = count + ' - ';
+    previousEntries.textContent += temp;
+    count = 0;
+    peopleCounter.textContent = count;
+}
+
+function finish() {
+    let totalNumberOfCustomers = document.getElementById('total-number-of-customers');
+
+
+    //The regular expression \d+ matches one or more digits, 
+    //and the g flag ensures that all matches are returned as an array.
+    let entries = previousEntries.innerHTML.match(/\d+/g);
+    let total = 0;
+    for (let i = 0; i < entries.length; i++) {
+        total += parseInt(entries[i]);
+    }
+    totalNumberOfCustomers.innerHTML += total;
+
+    document.querySelector('#add-button').disabled = true;
+    document.querySelector('#save-button').disabled = true;
+    document.querySelector('#finish-button').disabled = true;
+}
